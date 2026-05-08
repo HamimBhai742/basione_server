ad748ac989e4f94f70488d65c438c998051fd6cf
HamimBhai742 <mdhamim5088@gmail.com>
2026-05-08 17:40:06 +0600

    Update cors updated

--- src/app/utils/setCookies.ts
+++ src/app/utils/setCookies.ts
@@ -3,11 +3,19 @@ import config from "../../config";
 
 export const setCookies = (res: Response, token: { accessToken: string }) => {
   if (token.accessToken) {
+    // res.cookie("accessToken", token.accessToken, {
+    //   httpOnly: true,
+    //   secure: true,
+    //   sameSite: "none",
+    //   path: "/",
+    // });
+
     res.cookie("accessToken", token.accessToken, {
       httpOnly: true,
-      secure: true,
-      sameSite: "none",
+      secure: false,
+      sameSite: "lax",
       path: "/",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
   }
 };
