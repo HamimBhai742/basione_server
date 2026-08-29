4e59a8fc978ec305c7d5d2b405b0d128bc861954
HamimBhai742 <mdhamim5088@gmail.com>
2026-08-29 17:20:58 +0600

    add environment variable configuration module with centralized application settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,4 @@
 import "dotenv/config";
-import { cloudinary } from "../app/lib/cloudinary";
 export default {
   port: Number(process.env.PORT) || 3000,
   password_salt: Number(process.env.PASSWORD_SALT) || 10,
